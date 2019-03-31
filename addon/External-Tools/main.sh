9bdc58cac9a8941507e6f90e9826c73ab791a390
Zackptg5 <5107713+Zackptg5@users.noreply.github.com>
2019-03-31 16:08:25 -0400

    compress addons - git keeps corrupting magiskboot

--- addon/External-Tools/main.sh
+++
@@ -1,5 +0,0 @@
-# External Tools
-
-chmod -R 0755 $TMPDIR/addon/External-Tools
-cp -R $TMPDIR/addon/External-Tools/tools $UF 2>/dev/null
-[ -d "$UF/tools/other" ] && PATH=$UF/tools/other:$PATH
--- addon/Ramdisk-Patcher/uninstall.sh
+++
@@ -1,4 +0,0 @@
-[ -d "$RD" ] || unpack_ramdisk
-uninstall_files $INFORD "~"
-sed -i "/#$MODID-UnityIndicator/d" $RD/init.rc
-. $TMPDIR/addon/Ramdisk-Patcher/ramdiskuninstall.sh
